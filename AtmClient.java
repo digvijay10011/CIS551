756af0f881de3e204055a3f8b5a97ba28b2b660a
Digvijay <digvijay10011@gmail.com>
2017-11-07 02:06:07 -0500

    initial commit

---
+++ AtmClient.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.json.*;
+
+class AtmClient {
+
+    public static void main(String[] args) throws IOException {
+
+        //somehow values from the command line arguments should br loaded
+        //into the following variables which will then be passed to the bank.
+        String mode = "w";
+        String account = "bobby";
+        String amount = "20";
+        String cardfile = account + ".card";
+        int port = 3000;
+        String IPaddress = "127.0.0.1";
+        String authFile = "bank.auth";
+
+        JsonObject value = Json.createObjectBuilder().add("mode", mode).add("account", account).add("amount", amount).add("cardfile", cardfile).add("port", port).add("IPaddress", IPaddress).add("authFile", authFile).build();
+
+        Socket serverSocket = null;
+        PrintWriter pw = null;
+        BufferedReader br; 
+        
+        try {
+            serverSocket = new Socket(IPaddress, port);
+            pw = new PrintWriter(serverSocket.getOutputStream());
+            br = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+            
+            //passing the json object as string
+            pw.println(value.toString());
+            pw.flush();
+            
+            //getting response from the server. If in the response, "error" = true, then the transaction failed due to wrong inputs.
+            String response = null;
+            while((response=br.readLine()) == null);
+                
+            System.out.println(response);
+
+            pw.close();
+            br.close();
+            serverSocket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+    }
+}
---
+++ BankServer.java
@@ -0,0 +1,182 @@
+
+import javax.json.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class BankServer {
+
+    static HashMap<String, Double> balance = null;
+    static HashMap<String, Account> allAccounts = null;
+    static HashMap<String, String> cardFiles = null;
+
+    public static void main(String[] args) {
+
+        allAccounts = new HashMap<String, Account>();
+        cardFiles = new HashMap<String, String>();   //we will need a separate HashMap for cardFiles !
+                                                    // because we need to check if cardFileName is duplicated or not.
+
+        char mode ='n';
+        String requestedAccount = "";
+        double requestedAmount = 0.0;
+        String requestedCardFileName ="";
+        int port = 3000;
+        String authFile = "";
+        String cardFileContent = "";
+
+        ServerSocket server = null;
+        Socket clientSocket = null;
+        
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException ex) {
+            //handle it !!!!!
+        }
+        
+        while (true) {
+            try {
+                
+                clientSocket = server.accept();
+                
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
+                
+                String str = br.readLine();  //get the commands from client (basically, a JSON string)
+                
+                JsonReader jsonReader = Json.createReader(new StringReader(str));
+                JsonObject object = jsonReader.readObject();
+                jsonReader.close();
+
+                mode = object.getString("mode").charAt(0);
+                requestedAccount = object.getString("account");
+                requestedAmount = Double.parseDouble(object.getString("amount"));
+                requestedCardFileName = object.getString("cardfile");
+                authFile = object.getString("authFile");
+                //cardFileContent isn't being sent yet
+
+                Account account = null;
+                boolean error = false;
+                
+                switch (mode) {
+
+                    case 'n':   //new account
+
+                        if (allAccounts.get(requestedAccount) != null || requestedAmount < 10 || cardFiles.get(requestedCardFileName) != null) {
+                            error = true;
+                            break;
+                        }
+
+                        account = new Account();
+                        account.balance = requestedAmount;
+                        account.cardFileName = requestedCardFileName;
+
+                        allAccounts.put(requestedAccount, account);
+                        cardFiles.put(requestedCardFileName, cardFileContent);
+
+                        break;
+
+                    case 'w':    //withdraw
+
+                        account = allAccounts.get(requestedAccount);
+
+                        if (account == null) {
+                            error = true;
+                            break;
+                        }
+
+                        if (account.cardFileName.compareTo(requestedCardFileName) != 0 || !checkCardFiles(cardFiles.get(requestedCardFileName), cardFileContent) || requestedAmount <= 0) {
+                            error = true;
+                            break;
+                        }
+
+                        account.balance -= requestedAmount;
+
+                        if (account.balance < 0) {
+                            error = true;
+                        }
+
+                        break;
+
+                    case 'd':  //deposit
+
+                        account = allAccounts.get(requestedAccount);
+
+                        if (account == null) {
+                            error = true;
+                            break;
+                        }
+
+                        if (account.cardFileName.compareTo(requestedCardFileName) != 0 || !checkCardFiles(cardFiles.get(requestedCardFileName), cardFileContent) || requestedAmount <= 0) {
+                            error = true;
+                            break;
+                        }
+
+                        account.balance += requestedAmount;
+                        //what about max balance ??
+
+                        break;
+
+                    case 'g':   //get the balance
+
+                        account = allAccounts.get(requestedAccount);
+
+                        if (account == null) {
+                            error = true;
+                            break;
+                        }
+
+                        if (account.cardFileName.compareTo(requestedCardFileName) != 0 || !checkCardFiles(cardFiles.get(requestedCardFileName), cardFileContent) || requestedAmount <= 0) {
+                            error = true;
+                        }
+
+                        break;
+
+                }
+                
+                JsonObject response = null;
+                if(error){
+                    response = Json.createObjectBuilder().add("error", true).build();
+                }
+                else{
+                    response = Json.createObjectBuilder().add("error", false).add("account", requestedAccount).add("balance", Double.toString(account.balance)).build();
+                    
+                }
+                pw.println(response.toString());
+                pw.flush();
+                System.out.println(response.toString());
+
+                br.close();
+                pw.close();
+                clientSocket.close();
+
+            } catch (Exception e) {
+                System.out.println("error!");
+                //everytime a client disconnects, exception will be thrown
+            }
+        }
+
+    }
+
+    static boolean checkCardFiles(String actual, String current) {
+
+        //code to check the cardfiles
+        // return true if cardfiles match
+        return true;
+    }
+
+}
+
+class Account {
+
+    double balance;
+    String cardFileName;
+
+}
