425e549358f864427dafd1cfdf154160ab8593d5
Arjun Pothukuchi <apoth@seas.upenn.edu>
2017-11-09 18:57:32 -0500

    Add Command Line Parsing
    Use the commons-cli library to parse the command line arguments in
    ATMClient. The build file has been updated to include the new jar file.

--- AtmClient.java
+++ AtmClient.java
@@ -5,31 +5,98 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.Socket;
+import org.apache.commons.cli.*;
 import javax.json.*;
 
 class AtmClient {
 
+    private static void generateOption(Options options, String opt,
+                                    boolean hasArg, String description,
+                                    boolean isRequired) {
+        Option input = new Option(opt, hasArg, description);
+        input.setRequired(isRequired);
+        options.addOption(input);
+    }
+
+    private static String getOptionValue(CommandLine cmd, char opt, String def){
+        if (cmd.hasOption(opt)) {
+            return cmd.getOptionValue(opt);
+        }
+        return def;
+    }
+
+    private static void printInvalidArgs(Options options) {
+        (new HelpFormatter()).printHelp("atm", options);
+        System.exit(255);
+        return;
+    }
+
     public static void main(String[] args) throws IOException {
+        Options options = new Options();
+        
+        generateOption(options, "a", true, "account", true);
+        generateOption(options, "s", true, "auth-file", false);
+        generateOption(options, "i", true, "ip-address", false);
+        generateOption(options, "p", true, "port", false);
+        generateOption(options, "c", true, "card-file", false);
 
-        //somehow values from the command line arguments should br loaded
-        //into the following variables which will then be passed to the bank.
+        OptionGroup optionGroup = new OptionGroup();
+        optionGroup.addOption(new Option("n", false, "new-account"));
+        optionGroup.addOption(new Option("d", true, "deposit"));
+        optionGroup.addOption(new Option("w", true, "withdraw"));
+        optionGroup.addOption(new Option("g", true, "balance"));
+        optionGroup.setRequired(true);
+        options.addOptionGroup(optionGroup);
 
-        String mode = "n";
-        String account = "bobby";
-        double amount = 10;
-        String cardfile = account + ".card";
-        int port = 3000;
-        String IPaddress = "127.0.0.1";
-        String authFile = "bank.auth";
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
 
-        JsonObject value = Json.createObjectBuilder().add("mode", mode).add("account", account).add("amount", amount).add("cardfile", cardfile).add("port", port).add("IPaddress", IPaddress).add("authFile", authFile).build();
+        try {
+            cmd = parser.parse(options, args);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            printInvalidArgs(options);
+        } 
+
+        String account = cmd.getOptionValue('a');
+        String authFile = getOptionValue(cmd, 's', "bank.auth");
+        String ipAddress = getOptionValue(cmd, 'i', "127.0.0.1");
+        int port = Integer.valueOf(getOptionValue(cmd, 'p', "3000"));
+        String cardFile = getOptionValue(cmd, 'c', account + ".card");
+        String mode = "";
+        double amount = 0;
+        if (cmd.hasOption('n')) {
+            mode = "n";
+            amount = Double.valueOf(cmd.getOptionValue('n'));
+        } else if (cmd.hasOption('d')) {
+            mode = "d";
+            amount = Double.valueOf(cmd.getOptionValue('d'));
+        } else if (cmd.hasOption('w')) {
+            mode = "w";
+            amount = Double.valueOf(cmd.getOptionValue('w'));
+        } else if (cmd.hasOption('g')) {
+            mode = "g";
+            amount = Double.valueOf(cmd.getOptionValue('g'));
+        } else {
+            printInvalidArgs(options);
+        }
+
+        System.out.println(account);
+        System.out.println(authFile);
+        System.out.println(ipAddress);
+        System.out.println(port);
+        System.out.println(cardFile);
+        System.out.println(mode);
+        System.out.println(amount);
+
+        JsonObject value = Json.createObjectBuilder().add("mode", mode).add("account", account).add("amount", amount).add("cardfile", cardFile).add("port", port).add("IPaddress", authFile).add("authFile", authFile).build();
 
         Socket serverSocket = null;
         PrintWriter pw = null;
         BufferedReader br; 
         
         try {
-            serverSocket = new Socket(IPaddress, port);
+            serverSocket = new Socket(authFile, port);
             pw = new PrintWriter(serverSocket.getOutputStream());
             br = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
             
